0276dd2e8f21ff344c789139c81dae23b845fd14
Podvornaya Ksenia <dazaiosamu8586@gmail.com>
2026-03-25 00:38:23 +0700

    тесты треугольника

--- tests.cpp
+++ tests.cpp
@@ -1,1 +1,22 @@
 //test
+#include <iostream>
+#include <cassert>
+#include "Triangle.h"  
+
+using namespace std;
+
+int main() {
+    
+    cout << "Проверим для треугольника 3, 4, 5" << endl;
+    assert(perimetr(3, 4, 5) == 12);
+    assert(ploschad(3, 4, 5) == 6.0);
+    assert(ravnobedr(3, 4, 5) == false);
+
+    cout << "Для треугольника 5, 5, 6" << endl;
+    assert(perimetr(5, 5, 6) == 16);
+    assert(ploschad(5, 5, 6) == 12.0);
+    assert(ravnobedr(5, 5, 6) == true);
+
+    cout << "Все проверки прошли успешно!" << endl;
+    return 0;
+}
