08473143ef455413356849bf63f11ab3b44068e0
Podvornaya Ksenia <dazaiosamu8586@gmail.com>
2026-03-24 09:32:59 +0700

    Добавлен треугольник

---
+++ triangle.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double perimeter(double a, double b, double c) {
+    return a + b + c;
+}
+
+double ploschad(double a, double b, double c) {
+    double p = perimeter(a, b, c) / 2;
+    return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+bool ravnobedr(double a, double b, double c) {
+    return (a == b) || (a == c) || (b == c);
+}
+
+int main() {
+    double a, b, c;
+    cout << "Cтороны треугольника: ";
+    cin >> a >> b >> c;
+
+    if (a + b > c && a + c > b && b + c > a) {
+        cout << "Периметр: " << perimeter(a, b, c) << endl;
+        cout << "Площадь: " << ploschad(a, b, c) << endl;
+        if (ravnobedr(a, b, c))
+            cout << "Да, равнобедренный" << endl;
+        else
+            cout << "Нет" << endl;
+    } else {
+        cout << "Не существует" << endl;
+    }
+    return 0;
+}
