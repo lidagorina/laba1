d08dbf959bfb75ec8272a62f8f8a13ea30b1356b
Podvornaya Ksenia <dazaiosamu8586@gmail.com>
2026-03-25 23:37:26 +0700

    izmenenia

--- tests.cpp
+++ tests.cpp
@@ -1,38 +1,34 @@
 //test
+//test
 #include <iostream>
 #include <cassert>
 #include "Triangle.h"  
 #include "GorinaLida.h"
 using namespace std;
 
-
 int main() {
     
-
-	
-		
     cout << "Проверим для треугольника" << endl;
     int a, b, c;
     cin >> a >> b >> c;
 
-    if ((a <= 0) || (b <= 0) || (c <= 0))  {
-    	cout << "ne suschestvuet";
+    if ((a <= 0) || (b <= 0) || (c <= 0)) {
+        cout << "ne suschestvuet" << endl;
     }
-
     else {
-
-	 if ((a + b <  c) || (b +  c <  a) || (c + a <  b))  {
-         cout << "ne suschestvuet";
-	 }
-   
-	 else {
-	 assert(perimetr(a, b, c) == 12);
-   	 cout << perimetr(a, b, c) << endl;
-   	 assert(ploschad(a, b, c)  == 6.0);
-   	 cout << ploschad(a, b, c) << endl;
-   	 assert(ravnobedr(a, b, c) == false);
-   	 cout << ravnobedr(a, b, c) << endl;
-	 }
+        if ((a + b < c) || (b + c < a) || (c + a < b)) {
+            cout << "ne suschestvuet" << endl;
+        }
+        else {
+            assert(perimetr(a, b, c) == 12);
+            cout << perimetr(a, b, c) << endl;
+            assert(ploschad(a, b, c) == 6.0);
+            cout << ploschad(a, b, c) << endl;
+            assert(ravnobedr(a, b, c) == false);
+            cout << ravnobedr(a, b, c) << endl;
+        }
+	cout << "Проверка завершена" << endl;
     }
-      	
-
+    
+    return 0;
+}
